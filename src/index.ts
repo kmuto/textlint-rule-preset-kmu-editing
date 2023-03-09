ae44156f590fd2bd234607178ada757b66b75683
Kenshi Muto <kmuto@kmuto.jp>
2023-03-10 00:42:32 +0900

    add dictonary

--- src/index.ts
+++ src/index.ts
@@ -14,43 +14,8 @@ module.exports = {
         ...jaSpacingRules
     },
     rulesConfig: {
-/*        "preset-ja-technical-writing": true,
-        "rule-no-synonyms": true,
-        "preset-ja-engineering-paper": true,
-        "filter-rule-comments": true, */
-//        "no-kangxi-radicals": true,
-        // "preset-ja-spacing": true
-    }
-};
-
-/*
-import type { TextlintRuleModule } from "@textlint/types";
-export interface Options {
-    // If node's text includes allowed text, does not report.
-    allows?: string[];
-}
-
-const report: TextlintRuleModule<Options> = (context, options = {}) => {
-    const { Syntax, RuleError, report, getSource, locator } = context;
-    const allows = options.allows ?? [];
-    return {
-        [Syntax.Str](node) { // "Str" node
-            const text = getSource(node); // Get text
-            if (allows.some(allow => text.includes(allow))) {
-                return;
-            }
-            const matches = text.matchAll(/bugs/g);
-            for (const match of matches) {
-                const index = match.index ?? 0;
-                const matchRange = [index, index + match[0].length] as const;
-                const ruleError = new RuleError("Found bugs.", {
-                    padding: locator.range(matchRange)
-                });
-                report(node, ruleError);
-            }
+        "prh": {
+            "rulePaths": [__dirname + "/../dict/prh-dic.yml"]
         }
     }
 };
-
-export default report;
-*/
