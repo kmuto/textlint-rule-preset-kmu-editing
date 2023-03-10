98cb0de0bb1af8181c69fa57e30d249ea18680a0
Kenshi Muto <kmuto@hatena.ne.jp>
2023-03-10 14:34:19 +0900

    disable unify-kuten-and-touten

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,38 @@ module.exports = {
         ...jaSpacingRules
     },
     rulesConfig: {
+        "sentence-length": {
+            "max": 160 // 行文字数緩和
+        },
+        "max-comma": false, // カンマ・読点の数の制限なし
+        "max-ten": false,
+        "max-kanji-continuous-len": false, // 漢字最大長。ひとまず自動検査では不要判断
+        "no-mix-dearu-desumasu": {
+            "preferInHeader": "",
+            "preferInBody": "ですます",
+            "preferInList": "である",
+            "strict": false
+        },
+        "ja-no-mixed-period": {
+            "allowPeriodMarks": ["〜", "…", "："],
+            "allowEmojiAtEnd": true
+        },
+        /*"unify-kuten-and-touten": {
+            "kuten": "、",
+            "touten": "。"
+        },*/
+        "unify-kuten-and-touten": false, // うまく効いてくれないのでオフにしてみる
+        "no-doubled-joshi": false, // 助詞連続防止は調整困難
+        "no-exclamation-question-mark": false, // !,?は許可したい
+        "ja-no-weak-phrase": false, // かもしれない・思います アラートはやりすぎ
+        "ja-no-redundant-expression": false, // することができる アラートはやりすぎ(強調表現で許可したい)
+        "preset-ja-spacing": {
+            "ja-space-after-exclation": false, // !後の全角スペース限定やめ。本当は半角限定にしたい
+            "ja-space-after-question": false // ?後も同様
+        },
+        "ja-hiragana-keishikimeishi": {
+            "detection_ue": false // うえはむしろ漢字
+        },
         "prh": {
             "rulePaths": [__dirname + "/../dict/prh-dic.yml"]
         }
