15dc6c9bee3893dc7d8d03d9aa5cbb268377684b
Kenshi Muto <kmuto@kmuto.jp>
2023-03-12 20:48:25 +0900

    add termcheck

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const jaSpacingRules = moduleInterop(require("textlint-rule-preset-ja-spacing"))
 
 module.exports = {
     rules: {
+        "kmu-termcheck": moduleInterop(require("@kmuto/textlint-rule-kmu-termcheck")),
         "rule-no-synonyms": moduleInterop(require("@textlint-ja/textlint-rule-no-synonyms")),
         "filter-rule-comments": moduleInterop(require("textlint-filter-rule-comments")),
         "no-kangxi-radicals": moduleInterop(require("textlint-rule-no-kangxi-radicals")),
@@ -48,6 +49,9 @@ module.exports = {
         },
         "prh": {
             "rulePaths": [__dirname + "/../dict/prh-dic.yml"]
+        },
+        "kmu-termcheck": {
+            "severity": "warning"
         }
     }
 };
